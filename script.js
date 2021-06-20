2caf685c816b64e869849789efa053b1ddda1eea
Tanya G <cgomwe@ymail.com>
2021-06-20 14:07:57 +0100

    add favicon to html pages

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Here we are going to create a function that will play the sound when key is pressed
 window.addEventListener('keydown', function (e) {
   const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
   /**
    * A querySelector returns the first element within the document that matches the specified selector. null is returned if there is no match
    * data-key is custom attribute. rather than use a class selector we use the attribute selector as you would in css.
@@ -10,4 +11,5 @@ window.addEventListener('keydown', function (e) {
   if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; // rewind to the start
   audio.play();
+  console.log(key);
 });
