eaecbb100c91d67da810c6faa9b4849a00b2afba
Tanya G <cgomwe@ymail.com>
2021-06-20 15:30:15 +0100

    add remove transition function

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 // Here we are going to create a function that will play the sound when key is pressed
-window.addEventListener('keydown', function (e) {
-  const audio = this.document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  const key = this.document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+function playSound(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
   /**
    * A querySelector returns the first element within the document that matches the specified selector. null is returned if there is no match
-   * data-key is custom attribute. rather than use a class selector we use the attribute selector as you would in css.
-   * keyCode is depreciated, a better alternative is key but only if the browser supports it. keyCode will still work.
+   * data-key is a custom attribute. rather than use a class selector we use the attribute selector as you would in css.
+   * keyCode is depreciated, a better alternative is KEY but only if the browser supports it. keyCode will still work.
    * https://developer.mozilla.org/en-US/docs/Web/API/KeyboardEvent
    * */
   if (!audio) return; // stop the function from running all together
   audio.currentTime = 0; // rewind to the start
   audio.play();
-  console.log(key);
-});
+  key.classList.add('playing'); // highlight keys when pressed
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return; // return it if it's not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
